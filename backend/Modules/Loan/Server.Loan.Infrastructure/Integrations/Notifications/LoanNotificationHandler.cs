c4fa088f93f6d3b7841111b451946d4f5b1ba91a
RickDev3000 <81363560+SrRickGrimes@users.noreply.github.com>
2025-04-17 10:42:15 +0900

    added LoanNotification Handler

---
+++ backend/Modules/Loan/Server.Loan.Infrastructure/Integrations/Notifications/LoanNotificationHandler.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using Server.Loan.Contracts.Features.Loan.Notifications;
+
+namespace Server.Loan.Infrastructure.Integrations.Notifications;
+
+internal class LoanNotificationHandler:IEventHandler<LoanNotification>
+{
+    public Task HandleAsync(LoanNotification notification, CancellationToken cancellationToken)
+    {
+        // Handle the loan notification here
+        // For example, send an email or push notification
+        // we could apply outbox pattern here to send the notification to a queue or a service bus
+
+        return Task.CompletedTask;
+    }
+}
