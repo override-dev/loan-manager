c8955e7704d4585da8d6407f192d8ac4f1b85a1b
RickDev3000 <81363560+SrRickGrimes@users.noreply.github.com>
2025-04-17 15:38:35 +0900

    added bff project

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -5,4 +5,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Server>("server")
     .WithExternalHttpEndpoints();
 
+builder.AddProject<Projects.Bff>("bff");
+
 builder.Build().Run();
